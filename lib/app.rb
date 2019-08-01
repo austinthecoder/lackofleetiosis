7ecdd5ec106a7608c1e7425acce6e9dd82ba686c
Austin Schneider <me@austinschneider.com>
2019-08-01 10:44:20 -0400

    refactor/simplify result objects

--- lib/app.rb
+++ lib/app.rb
@@ -7,7 +7,7 @@ class App
     vin = vin.to_s.strip
 
     if vin.blank?
-      return Result.new(status: :unprocessable_entity, errors: ['VIN is required.'])
+      return Ivo.(status: :unprocessable_entity, errors: ['VIN is required.'])
     end
 
     fleetio_api = HTTP.headers(
@@ -18,14 +18,14 @@ class App
     resp = fleetio_api.get('https://secure.fleetio.com/api/v1/vehicles')
 
     if resp.code != 200
-      return Result.new(status: :service_unavailable, errors: ['Service is unavailable at the moment.'])
+      return Ivo.(status: :service_unavailable, errors: ['Service is unavailable at the moment.'])
     end
 
     fleetio_vehicles = JSON.parse(resp.body.to_s, symbolize_names: true)
     fleetio_vehicle = fleetio_vehicles.find { |v| v[:vin] == vin }
 
     unless fleetio_vehicle
-      return Result.new(status: :unprocessable_entity, errors: ['Unable to identify a vehicle.'])
+      return Ivo.(status: :unprocessable_entity, errors: ['Unable to identify a vehicle.'])
     end
 
     vehicle = Vehicle.new(
@@ -39,9 +39,9 @@ class App
     )
 
     if vehicle.save
-      Result.new(status: :created, id: vehicle.id)
+      Ivo.(status: :created, id: vehicle.id)
     else
-      Result.new(status: :unprocessable_entity, errors: vehicle.errors.values.flatten)
+      Ivo.(status: :unprocessable_entity, errors: vehicle.errors.values.flatten)
     end
   end
 
@@ -50,16 +50,16 @@ class App
       build_vehicle_resource(vehicle)
     end
 
-    Result.new(status: :ok, resources: resources)
+    Ivo.(status: :ok, resources: resources)
   end
 
   def fetch_vehicle(id:)
     vehicle = Vehicle.find_by(id: id)
 
     if vehicle
-      Result.new(status: :ok, resource: build_vehicle_resource(vehicle))
+      Ivo.(status: :ok, resource: build_vehicle_resource(vehicle))
     else
-      Result.new(status: :not_found)
+      Ivo.(status: :not_found)
     end
   end
 
