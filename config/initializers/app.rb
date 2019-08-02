7cc923bdf002f71738301a325dd4465483c289c7
Austin Schneider <me@austinschneider.com>
2019-08-02 13:28:08 -0400

    fix reloads

--- config/initializers/app.rb
+++ config/initializers/app.rb
@@ -1,1 +1,5 @@
-$app = App.start
+starter = -> { $app = App.start }
+starter.()
+
+# avoids the error: ArgumentError (A copy of App has been removed from the module tree but is still active!)
+ActiveSupport::Reloader.to_prepare(&starter)
