1030f74d4721cdef6252ce43a302cfa1c190d86c
Austin Schneider <me@austinschneider.com>
2019-08-02 13:28:27 -0400

    improve fleetio api error handling

--- lib/fleetio.rb
+++ lib/fleetio.rb
@@ -15,9 +15,7 @@ class Fleetio
   def fetch_vehicle(vin:)
     resp = api.get('https://secure.fleetio.com/api/v1/vehicles')
 
-    if resp.code != 200
-      return Ivo.(status: :service_unavailable, errors: ['Service is unavailable at the moment.'])
-    end
+    return service_unavailable if resp.code != 200
 
     vehicles = JSON.parse(resp.body.to_s, symbolize_names: true)
     vehicle = vehicles.find { |v| v[:vin] == vin }
@@ -27,21 +25,27 @@ class Fleetio
     else
       Ivo.(status: :not_found)
     end
+  rescue HTTP::Error
+    service_unavailable
   end
 
   def fetch_fuel_entries(vehicle_id:)
     resp = api.get("https://secure.fleetio.com/api/v1/vehicles/#{vehicle_id}/fuel_entries")
 
-    if resp.code != 200
-      return Ivo.(status: :service_unavailable, errors: ['Service is unavailable at the moment.'])
-    end
+    return service_unavailable if resp.code != 200
 
     fuel_entries = JSON.parse(resp.body.to_s, symbolize_names: true)
 
     Ivo.(status: :ok, fuel_entries: fuel_entries)
+  rescue HTTP::Error
+    service_unavailable
   end
 
   private
 
   attr_reader :api
+
+  def service_unavailable
+    Ivo.(status: :service_unavailable, errors: ['Service is unavailable at the moment.'])
+  end
 end
