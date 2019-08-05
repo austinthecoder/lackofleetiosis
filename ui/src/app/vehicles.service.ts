e276cd4cb4da47b54e1f1c81fa81d1482164dc69
Austin Schneider <me@austinschneider.com>
2019-08-04 22:30:41 -0400

    make host configurable

--- ui/src/app/vehicles.service.ts
+++ ui/src/app/vehicles.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { Observable, of, throwError } from 'rxjs';
 import { catchError, tap, map } from 'rxjs/operators';
 import { Vehicle } from './vehicle';
+import { environment } from '../environments/environment';
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
@@ -15,19 +16,19 @@ export class VehiclesService {
   constructor(private http: HttpClient) {}
 
   add(vehicle: Vehicle): Observable<Vehicle> {
-    return this.http.post<Vehicle>('http://localhost:3000/vehicles', vehicle, httpOptions).pipe(
+    return this.http.post<Vehicle>(`${environment.apiHost}/vehicles`, vehicle, httpOptions).pipe(
       catchError(this.handleError)
     );
   }
 
   fetch(): Observable<Vehicle[]> {
-    return this.http.get<Vehicle[]>('http://localhost:3000/vehicles').pipe(
+    return this.http.get<Vehicle[]>(`${environment.apiHost}/vehicles`).pipe(
       catchError(this.handleError)
     );
   }
 
   reprocess(vehicle: Vehicle): Observable<any> {
-    return this.http.post(`http://localhost:3000/vehicles/${vehicle.id}/reprocess`, null).pipe(
+    return this.http.post(`${environment.apiHost}/vehicles/${vehicle.id}/reprocess`, null).pipe(
       catchError(this.handleError)
     );
   }
